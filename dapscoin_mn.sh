7fae3c91eb819626c8494af28a576cd24454e24e
nashsclay <nashsclay@gmail.com>
2019-10-29 00:10:29 -0700

    Update dapscoin_mn.sh

--- dapscoin_mn.sh
+++ dapscoin_mn.sh
@@ -8,7 +8,7 @@ COIN_DAEMON='dapscoind'
 COIN_CLI='dapscoin-cli'
 COIN_TX='dapscoin-tx'
 COIN_PATH='/usr/local/bin/'
-COIN_TGZ='https://github.com/DAPSCoin/DAPSCoin/releases/download/1.0.1/master_linux-v1.0.1.3.zip'
+COIN_TGZ='https://github.com/DAPSCoin/DAPSCoin/releases/download/1.0.3/master_linux-v1.0.3.4.zip'
 COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 COIN_NAME='DAPSCoin'
 COIN_PORT=53572
@@ -29,7 +29,7 @@ function download_node() {
   compile_error
   unzip $COIN_ZIP >/dev/null 2>&1
   #cd bin #verify
-  cd master_linux-v1.0.1.3
+  cd master_linux-v1.0.3.4
   sudo mv dapscoind dapscoin-cli dapscoin-tx /usr/local/bin
   cd /usr/local/bin/
   chmod +x $COIN_DAEMON $COIN_CLI $COIN_TX 
